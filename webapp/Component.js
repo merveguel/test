c60bd327132f7de24fcd0109cfd8ccc79701fd6d
Merve Gül <gulmmerve@gmail.com>
2023-01-02 19:13:02 +0100

    dummy comment

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,7 +2,8 @@ sap.ui.define(
     ["sap/suite/ui/generic/template/lib/AppComponent"],
     function (Component) {
         "use strict";
-
+        
+        // test
         return Component.extend("zmgtest.Component", {
             metadata: {
                 manifest: "json"
